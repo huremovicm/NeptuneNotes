f218a08e621bcf31cb64edfa7723ce4706792643
Maid Huremovic <maid@pop-os.localdomain>
2021-11-29 16:23:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/neptunenotes/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.neptunenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signupBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signupBtn = findViewById(R.id.signupBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
