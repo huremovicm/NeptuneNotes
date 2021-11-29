5ddfa8e7f782d075bb2d751f06500ae37525cfa7
huremovicm <maid.huremovic@outlook.com>
2021-11-29 21:02:16 +0100

    Sign Up form connected with database.

--- app/src/main/java/com/example/neptunenotes/MainActivity.java
+++ app/src/main/java/com/example/neptunenotes/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private Button signupBtn;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/neptunenotes/Signup.java
+++ app/src/main/java/com/example/neptunenotes/Signup.java
@@ -1,14 +1,121 @@
 package com.example.neptunenotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
+
+    private TextView createBtn;
+    private FirebaseAuth mAuth;
+    private TextInputEditText editTextEmail, editTextUsername, editTextPassword, confPassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        createBtn = (Button) findViewById(R.id.createAccount);
+
+        editTextEmail = (TextInputEditText) findViewById(R.id.emailSignup1);
+        editTextUsername = (TextInputEditText) findViewById(R.id.usernameSignup1);
+        editTextPassword = (TextInputEditText) findViewById(R.id.passwordSignup1);
+        confPassword = (TextInputEditText) findViewById(R.id.passwordSignupMatch1);
+
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  email = editTextEmail.getText().toString().trim();
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                String passwordconf = confPassword.getText().toString().trim();
+
+                if (username.isEmpty()) {
+                    editTextUsername.setError("Username is requried!");
+                    editTextUsername.requestFocus();
+                    return;
+
+                }
+                if (email.isEmpty()) {
+                    editTextEmail.setError("Email is requried!");
+                    editTextEmail.requestFocus();
+                    return;
+
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                   editTextEmail.setError("Please provide valid email!");
+                    editTextEmail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    editTextPassword.setError("Password is requried!");
+                    editTextPassword.requestFocus();
+                    return;
+
+                }
+                if (password.length() < 6) {
+                    editTextPassword.setError("Minimum password length should be 6 characters!");
+                    editTextPassword.requestFocus();
+                    return;
+                }
+                if (passwordconf.isEmpty()) {
+                    confPassword.setError("Password is requried!");
+                    confPassword.requestFocus();
+                    return;
+
+                }
+                if (passwordconf.length() < 6) {
+                    confPassword.setError("Minimum password length should be 6 characters!");
+                    confPassword.requestFocus();
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                       if (task.isSuccessful()){
+                           User user_ = new User(email, username);
+
+                           FirebaseDatabase.getInstance().getReference("Users")
+                                   .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                   .setValue(user_).addOnCompleteListener(new OnCompleteListener<Void>() {
+                               @Override
+                               public void onComplete(@NonNull Task<Void> task) {
+                                   if (task.isSuccessful()){
+                                       Toast.makeText(Signup.this, "User has been signed up!", Toast.LENGTH_SHORT).show();
+                                   }else{
+                                       Toast.makeText(Signup.this, "Faild to sign up user!", Toast.LENGTH_SHORT).show();
+                                   }
+                               }
+                           });
+                       }else{
+                           Toast.makeText(Signup.this, "Faild to sign up user!", Toast.LENGTH_SHORT).show();
+
+                       }
+                    }
+                });
+
+            }
+        });
     }
 }
